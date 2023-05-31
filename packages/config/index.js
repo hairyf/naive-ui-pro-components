5b27275ee43367005a54ee8d9f985b0f8d3ba6df
mrmao <wwu710632@gmail.com>
2023-05-31 18:15:45 +0800

    feat(config): iife format and remove iifeMin format
    This commit adds support for the iife format and removes the iifeMin format from the build options. The iife format is now included by default, allowing users to build their package as an immediately invoked function expression (IIFE). The iifeMin format has been removed as it was redundant with the minify flag now enabled by default on the iife format. The esbuild plugin configuration has updated to include the unplugin-vue-jsx plugin in both the default and iife formats.

--- packages/config/index.js
+++ packages/config/index.js
@@ -2,8 +2,9 @@ const Globals = require('esbuild-plugin-globals')
 
 exports.defineBuildConfig = (options) => {
   const formats = options?.format || ['cjs', 'esm']
-  const iifeMin = formats.findIndex(v => v === 'iife-min')
-
+  const iife = formats.findIndex(v => v === 'iife')
+  formats.splice(iife, 1)
+  const iifeMin = formats.findIndex(v => v === 'iifeMin')
   formats.splice(iifeMin, 1)
 
   const configs = []
@@ -11,7 +12,6 @@ exports.defineBuildConfig = (options) => {
   options = {
     ...options,
     external: ['vue-demi', 'naive-ui', '@vueuse/core'],
-    esbuildPlugins: [Globals(options?.globals), require('unplugin-vue-jsx/esbuild')()],
     outExtension({ format }) {
       return { js: `.${format}.js` }
     },
@@ -19,11 +19,23 @@ exports.defineBuildConfig = (options) => {
 
   configs.push(options)
 
+  if (iife) {
+    configs.push({
+      ...options,
+      format: 'iife',
+      esbuildPlugins: [Globals(options?.globals), require('unplugin-vue-jsx/esbuild')()],
+      outExtension({ format }) {
+        return { js: `.${format}.js` }
+      },
+      dts: false,
+    })
+  }
   if (iifeMin) {
     configs.push({
       ...options,
       format: 'iife',
       minify: true,
+      esbuildPlugins: [Globals(options?.globals), require('unplugin-vue-jsx/esbuild')()],
       outExtension({ format }) {
         return { js: `.${format}.min.js` }
       },
