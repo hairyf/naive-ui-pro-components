c5a08009edef08ef7ec50591182baeba0104ebe3
Mr.Mao <951416545@qq.com>
2023-06-01 21:16:14 +0800

    docs(form): add array demo
    Added a new demo file to show how `defineForm` can be used in array mode to merge fields within an array. This allows for more flexibility in defining forms with complex data structures. The demo also includes a simple example of using a reactive reference for a form field value.

---
+++ docs/components/form/demo/array.vue
@@ -0,0 +1,27 @@
+<template>
+  <n-pro-form :instance="form" />
+  <pre>{{ JSON.stringify(form.dataTrans, null, 2) }}</pre>
+</template>
+
+<script lang="ts" setup>
+import { defineForm } from 'naive-ui-pro-components'
+
+import { ref } from 'vue'
+
+const email = ref('')
+
+const form = defineForm([
+  {
+    type: 'input',
+    key: 'brandNum',
+    placeholder: 'Please input brand number',
+    value: '',
+  },
+  {
+    type: 'input',
+    key: 'email',
+    placeholder: 'Please input email',
+    value: email,
+  },
+])
+</script>
