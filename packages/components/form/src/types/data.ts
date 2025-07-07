ebf01c9d7bdddc831169a0a14800bef0ef31cbc1
hairyf <wwu710632@gmail.com>
2025-07-07 21:24:10 +0800

    fix: correct type definitions and naming inconsistencies in data types

--- packages/components/form/src/types/data.ts
+++ packages/components/form/src/types/data.ts
@@ -3,25 +3,25 @@ import type { UnwrapRef } from 'vue'
 interface Field<T> { value: T }
 
 export type Data<T = Record<string, any>> = {
-  [K in keyof T as FilterReceives<T, K>]: UnwrapRef<FieldValue<T[K]>>;
+  [K in keyof T as FilterOmitReceive<T, K>]: UnwrapRef<FieldValue<T[K]>>;
 } & Record<string, any>
 
-export type TransformData<T extends Record<string, any> = Record<string, any>> = OmitTransormData<T> & Transform<PickTransorm<T>>
+export type TransformData<T extends Record<string, any> = Record<string, any>> = OmitTransformData<T> & Transform<PickTransform<T>>
 
 type Transform<T extends Record<string, any>> = Flatten<{ [K in keyof T]: ReturnType<NonNullable<T[K]['transform']>> }>
 
-type OmitTransormData<T> = {
-  [K in keyof T as FilterOmitTransormKeys<T, FilterReceives<T, K>>]: UnwrapRef<FieldValue<T[K]>>;
+type OmitTransformData<T> = {
+  [K in keyof T as FilterOmitTransformKeys<T, FilterOmitReceive<T, K>>]: UnwrapRef<FieldValue<T[K]>>;
 }
 
-type PickTransorm<T> = {
-  [K in keyof T as FilterPickTransormKeys<T, FilterReceives<T, K>>]: T[K];
+type PickTransform<T> = {
+  [K in keyof T as FilterPickTransformKeys<T, FilterOmitReceive<T, K>>]: T[K];
 }
 
 type FieldValue<T> = T extends Field<infer V> ? V : string
-type FilterOmitTransormKeys<T, K extends keyof T> = T[K] extends FieldTransform<any, any> ? never : K
-type FilterPickTransormKeys<T, K extends keyof T> = T[K] extends FieldTransform<any, any> ? K : never
-type FilterReceives<T, K extends keyof T> = T[K] extends { receive: false } ? never : K
+type FilterOmitTransformKeys<T, K extends keyof T> = T[K] extends FieldTransform<any, any> ? never : K
+type FilterPickTransformKeys<T, K extends keyof T> = T[K] extends FieldTransform<any, any> ? K : never
+type FilterOmitReceive<T, K extends keyof T> = T[K] extends { receive: false } ? never : K
 
 interface FieldTransform<V, R> {
   transform?: (value: V, key: string) => R
